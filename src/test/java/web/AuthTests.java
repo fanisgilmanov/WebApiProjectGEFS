74cce765d152a04a89d7f334aa210e0cf61760ce
FanisGilmanov <fanisgilmanov1994@gmail.com>
2022-11-11 18:56:53 +0500

    добавление browserSize

--- src/test/java/web/AuthTests.java
+++ src/test/java/web/AuthTests.java
@@ -1,20 +1,31 @@
 package web;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class AuthTests {
-
+    @BeforeAll
+    static void beforeUrl() {
+        Configuration.baseUrl = "http://client-login.stage.gboteam.ru";
+        Configuration.browserSize = "1920x1080";
+    }
 
     @Test
     void testAuth() {
-        open("http://client-login.stage.gboteam.ru");
+        open(baseUrl);
         $("[name=login]").setValue("testuser99");
         $("[type=password]").setValue("12345678gG!");
         $("[type=submit]").click();
-        $("[name=code]").setValue("111111");
+        $("[name=code]").shouldBe(visible, Duration.ofSeconds(10)).setValue("111111");
         $("[type=submit]").click();
     }
 }
